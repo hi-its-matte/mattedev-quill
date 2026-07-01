703bb439f1a0ee7d53324e7cdffef79a692fe342
hi-its-matte <139715336+hi-its-matte@users.noreply.github.com>
2026-07-01 15:41:24 +0200

    Replaced Placeholder

--- assets/js/login/firebase.js
+++ assets/js/login/firebase.js
@@ -2,9 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "XXX",
-  authDomain: "mattedev.firebaseapp.com",
-  projectId: "mattedev",
+  apiKey: "AIzaSyC7Tbqt5FzJK8Z_USkCMWxXiHZp8uRN26A",
+  authDomain: "mattedev-account.firebaseapp.com",
+  projectId: "mattedev-account",
+  storageBucket: "mattedev-account.firebasestorage.app",
+  messagingSenderId: "77268069903",
+  appId: "1:77268069903:web:040aa6c3981eb3650afd7a"
 };
 
 const app = initializeApp(firebaseConfig);
